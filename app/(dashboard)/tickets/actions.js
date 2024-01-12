a7cb9fa8b06c762bedd8ec3b39e25829190f7f6a
jdwjdwjdw <jacobdeanwilson@gmail.com>
2024-01-12 12:00:27 -0800

    Handle POST request in server action

--- app/(dashboard)/tickets/actions.js
+++ app/(dashboard)/tickets/actions.js
@@ -1,5 +1,25 @@
 "use server"
 
+import { cookies } from "next/headers"
+
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs"
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
+
 export async function addTicket(formData) {
-  console.log('hello from the server action')
+  const ticket = Object.fromEntries(formData)
+  
+  const supabase = createServerActionClient({ cookies })
+
+  const { data: { session }} = await supabase.auth.getSession()
+
+  // insert the data
+  const { error } = await supabase.from('tickets')
+    .insert({
+      ...ticket,
+      user_email: session.user.email
+    })
+
+  revalidatePath('/tickets')
+  redirect('/tickets')
 }
